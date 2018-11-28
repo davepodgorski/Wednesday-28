764f17c074365d0a8f0d79486dcf5ad73a8cd2da
davepodgorski <davepodgorski@gmail.com>
2018-11-28 18:55:13 -0500

    my first website

--- server.rb
+++ server.rb
@@ -3,20 +3,40 @@ require 'sinatra'
       'Put this in your pipe & smoke it!'
     end
 
+    get '/gallery' do
+      redirect to('/portfolio')
+    end
+
+    get '/' do
+      redirect to('/home')
+    end
     get '/portfolio' do
       erb :gallery
     end
 
     get '/about_me' do
+      @skills = ['git', 'HTML', 'CSS', 'Ruby']
+      @interests = ['cats', 'art', 'music', 'films', 'coffee']
       erb :about_me
     end
 
     get '/home' do
-  erb :index
-end
+      erb :index
+    end
 
     get '/hello' do
       "Hello Dave"
+    end
+
+    get '/favorites' do
+      @fav_links = [
+        "https://slashdot.org/",
+        "https://games.slashdot.org/",
+        "https://www.cbc.ca/",
+        "http://www.angryflower.com/",
+        "http://www.innatthecrossroads.com/"
+      ]
+      erb :favorites
     end
 
     get '/greetings' do
